e9c4526f91052925a431f74e3d746ea9afa46ac7
unknown <xynd@icloud.com>
2019-02-04 18:36:45 +0600

    it was so hard

---
+++ Week 2/Task 1/Task 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int a = 1;
+            int b = s.Length;
+            for(int i=0; i<s.Length;i++)
+            {
+                if (s[i] != s[b - i-1])
+                {
+                    a++;
+                }
+                    
+            }
+            if (a==1)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
